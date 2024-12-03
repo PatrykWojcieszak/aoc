b9ee8c51b175fc1da086555a5f167ef26b01af3c
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2024-12-03 11:14:49 +0100

    feat: Day 03

---
+++ 2024/src/Day03/part1/code.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+
+	"aoc/2024/pkg/file"
+)
+
+func regexFilter(line []string) []string {
+	pattern := `mul\(\d{1,3},\d{1,3}\)`
+
+	var validMul []string
+
+	for _, value := range line {
+		regex := regexp.MustCompile(pattern)
+		matches := regex.FindAllString(value, -1)
+
+		validMul = append(validMul, matches...)
+	}
+
+	return validMul
+}
+
+func multiply(instr []string) int {
+	pattern := `\d+`
+
+	result := 0
+
+	for _, value := range instr {
+		regex := regexp.MustCompile(pattern)
+		matches := regex.FindAllString(value, -1)
+
+		num1, _ := strconv.Atoi((matches[0]))
+		num2, _ := strconv.Atoi((matches[1]))
+
+		result = result + num1*num2
+	}
+
+	return result
+}
+
+func main() {
+	absPathName, _ := filepath.Abs("input.txt")
+	output, _ := file.ReadInput(absPathName)
+
+	validInstr := regexFilter(output)
+	fmt.Println(multiply(validInstr))
+}
---
+++ 2024/src/Day03/part2/code.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"aoc/2024/pkg/file"
+)
+
+func regexFilter(line []string) []string {
+	pattern := `(?:mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\))`
+
+	var validMul []string
+
+	for _, value := range line {
+		regex := regexp.MustCompile(pattern)
+		matches := regex.FindAllString(value, -1)
+
+		validMul = append(validMul, matches...)
+	}
+
+	return validMul
+}
+
+func multiply(instr []string) int {
+	numbersPattern := `\d+`
+	dontPattern := `don't`
+	doPattern := `do`
+	shouldMultiply := true
+	result := 0
+
+	for _, value := range instr {
+		regex := regexp.MustCompile(numbersPattern)
+		matches := regex.FindAllString(value, -1)
+
+		if len(matches) == 0 && strings.Contains(value, dontPattern) {
+			shouldMultiply = false
+		} else if len(matches) == 0 && strings.Contains(value, doPattern) {
+			shouldMultiply = true
+		}
+
+		if shouldMultiply && len(matches) > 1 {
+			num1, _ := strconv.Atoi((matches[0]))
+			num2, _ := strconv.Atoi((matches[1]))
+
+			result = result + num1*num2
+		}
+	}
+
+	return result
+}
+
+func main() {
+	absPathName, _ := filepath.Abs("input.txt")
+	output, _ := file.ReadInput(absPathName)
+
+	validInstr := regexFilter(output)
+	// fmt.Println(validInstr)
+	fmt.Println(multiply(validInstr))
+}
