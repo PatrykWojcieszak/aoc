732fbf57ceb8334c4cc9ddc00e453bc810e68136
Patryk Wojcieszak <patryk.p.wojcieszak@gmail.com>
2024-12-14 14:20:25 +0100

    feat: optimize Day14 part1

--- 2024/src/Day14/part1/code.go
+++ 2024/src/Day14/part1/code.go
@@ -11,10 +11,12 @@ func main() {
 	absPathName, _ := filepath.Abs("input.txt")
 	output, _ := file.ReadInput(absPathName)
 
-	grid := [103][101]int{}
 	height := 103
 	width := 101
 	seconds := 100
+	halfX := (width - 1) / 2
+	halfY := (height - 1) / 2
+	q1, q2, q3, q4 := 0, 0, 0, 0
 
 	for i := 0; i < len(output); i += 1 {
 		var px, py, vx, vy int
@@ -22,32 +24,22 @@ func main() {
 
 		newX := ((px+seconds*vx)%width + width) % width
 		newY := ((py+seconds*vy)%height + height) % height
-		grid[newY][newX] += 1
-	}
 
-	q1, q2, q3, q4 := 0, 0, 0, 0
-	halfX := (width - 1) / 2
-	halfY := (height - 1) / 2
-
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			if x < halfX && y < halfY {
-				q1 += grid[y][x]
-			}
-
-			if x > halfX && y < halfY {
-				q2 += grid[y][x]
-			}
-
-			if x < halfX && y > halfY {
-				q3 += grid[y][x]
-			}
-
-			if x > halfX && y > halfY {
-				q4 += grid[y][x]
-			}
+		if newX < halfX && newY < halfY {
+			q1++
 		}
 
+		if newX > halfX && newY < halfY {
+			q2++
+		}
+
+		if newX < halfX && newY > halfY {
+			q3++
+		}
+
+		if newX > halfX && newY > halfY {
+			q4++
+		}
 	}
 
 	fmt.Println("Result:", q1*q2*q3*q4)
